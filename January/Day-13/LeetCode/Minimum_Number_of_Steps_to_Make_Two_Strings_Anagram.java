ce51796d04f757751b07935b1fa1b9175102ffae
Miraj Asraf <mirajasraf786@gmail.com>
2024-01-13 19:11:16 +0530

    Minimum Number of Steps to Make Two Strings Anagram

--- January/Day-13/LeetCode/Minimum_Number_of_Steps_to_Make_Two_Strings_Anagram.java
+++ January/Day-13/LeetCode/Minimum_Number_of_Steps_to_Make_Two_Strings_Anagram.java
@@ -1,1 +1,17 @@
+class Solution {
+    public int minSteps(String s, String t) {
+        int[] count = new int[26];
 
+        for (int i = 0; i < s.length(); i++) {
+            count[t.charAt(i) - 'a']++;
+            count[s.charAt(i) - 'a']--;
+        }
+
+        int steps = 0;
+        for (int i = 0; i < 26; i++) {
+            steps += Math.max(0, count[i]);
+        }
+
+        return steps;
+    }
+}
