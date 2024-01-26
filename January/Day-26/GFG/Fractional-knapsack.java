c0d57cfee78a47d359b478d14b20836930f68020
Miraj Asraf <mirajasraf786@gmail.com>
2024-01-26 22:14:35 +0530

    Fractional Knapsack

---
+++ January/Day-26/GFG/Fractional-knapsack.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Item {
+    int value, weight;
+    Item(int x, int y){
+        this.value = x;
+        this.weight = y;
+    }
+}
+
+class Solution
+{
+    //Function to get the maximum total value in the knapsack.
+    double fractionalKnapsack(int W, Item arr[], int n) 
+    {
+        // Your code here
+        Arrays.sort(arr,(a,b)->Double.compare(b.value*1.0/b.weight,a.value*1.0/a.weight));//double compare is imp for correct ans
+        
+        double ans = 0;
+        int i=0;
+        
+        while(i<n && arr[i].weight<=W){
+            ans += arr[i].value;
+            W -= arr[i].weight;
+            i++;
+        }
+        
+        if(i<n && W>0)
+        ans += (arr[i].value*1.0/arr[i].weight) * W;
+            
+        return ans;
+    }
+}
