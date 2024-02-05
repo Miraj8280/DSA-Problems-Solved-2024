13bd3b91ba5ab617ec29fb0abfa4ee5be58d991e
Miraj Asraf <mirajasraf786@gmail.com>
2024-02-05 23:38:28 +0530

    Sorted insert for circular linked list

---
+++ February/Day-05/GFG/Sorted-insert-for-circular-linked-list.java
@@ -0,0 +1,25 @@
+class Solution {
+    public Node sortedInsert(Node head, int data) {
+        // code here
+        Node newNode = new Node(data);
+        
+        if(head==null){
+           newNode.next=newNode;
+           return newNode;
+        } 
+        
+        Node prev=head;
+        Node curr=head.next;
+         
+        while(curr!=head){
+            if(prev.data<=data && data<=curr.data) break;
+            prev=curr;
+            curr=curr.next;
+        }
+        
+        prev.next=newNode;
+        newNode.next=curr;
+
+        return (data<head.data) ? newNode : head;
+    }
+}
